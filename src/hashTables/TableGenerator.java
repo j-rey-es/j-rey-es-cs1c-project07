6a49bb48df9ddcc80ae9486df531a9b2f7f17750
Joel Reyes <its.j.reyes@gmail.com>
2020-06-10 01:07:51 -0700

    Added methods to TableGenerator

--- src/hashTables/TableGenerator.java
+++ src/hashTables/TableGenerator.java
@@ -2,10 +2,16 @@ package hashTables;
 
 import cs1c.SongEntry;
 
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
 public class TableGenerator {
+
+    private ArrayList<String> genreNames;
     public FHhashQPwFind<Integer,SongCompInt> populateIDtable(SongEntry[] songEntries)
     {
-        FHhashQPwFind<Integer,SongCompInt>  hashSongID =  new FHhashQPwFind<Integer,SongCompInt>();
+        FHhashQPwFind<Integer,SongCompInt> hashSongID =  new FHhashQPwFind<Integer,SongCompInt>();
+
         for (int i = 0; i < songEntries.length; i++)
         {
             SongCompInt song = new SongCompInt(songEntries[i]);
@@ -17,12 +23,27 @@ public class TableGenerator {
     public FHhashQPwFind<String,SongsCompGenre> populateGenreTable(SongEntry[] songEntries)
     {
         FHhashQPwFind<String,SongsCompGenre> hashSongGenre =  new FHhashQPwFind<String,SongsCompGenre>();
+
         for (int i = 0; i < songEntries.length; i++)
         {
-            SongsCompGenre song = new SongsCompGenre(songEntries[i]);
-            hashSongGenre.insert(song);
+            try
+            {
+                SongsCompGenre songGenre = hashSongGenre.find(songEntries[i].getGenre());
+                songGenre.addSong(songEntries[i]);
+            }
+            catch (NoSuchElementException e)
+            {
+                SongsCompGenre song = new SongsCompGenre(songEntries[i]);
+                hashSongGenre.insert(song);
+                genreNames.add(songEntries[i].getGenre());
+            }
+
         }
         return hashSongGenre;
+    }
+    public ArrayList<String> getGenreNames()
+    {
+        return genreNames;
     }
 
 }
